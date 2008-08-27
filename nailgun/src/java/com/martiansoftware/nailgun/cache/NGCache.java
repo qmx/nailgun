56ee57c184991292564dad46711f167cf0fc7ecd
nwhitehead <nwhitehead@ff0b44bd-d83d-4c2c-bf2c-1e3224db2823>
2008-08-27 13:29:05 +0000

    Started JMX annotations.

--- nailgun/src/java/com/martiansoftware/nailgun/cache/NGCache.java
+++ nailgun/src/java/com/martiansoftware/nailgun/cache/NGCache.java
@@ -23,27 +23,46 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.helios.jmx.dynamic.annotations.JMXAttribute;
+import org.helios.jmx.dynamic.annotations.JMXManagedObject;
+import org.helios.jmx.dynamic.annotations.JMXOperation;
+import org.helios.jmx.dynamic.annotations.JMXParameter;
+import org.helios.jmx.dynamic.annotations.options.AttributeMutabilityOption;
+
 /**
  * <p>Title: NGCache</p>
- * <p>Description: An in memory cache for arbitrary objects where nails can reference items in the NGServer.</p> 
+ * <p>Description: An in memory soft reference cache for arbitrary objects where nails can reference items in the NGServer.</p> 
  * <p>Copyright 2008, Martian Software, Inc.</p>
  * @author Whitehead (nwhitehead@heliosdev.org)
  * @version $Revision$
  */
-
+@JMXManagedObject (annotated=true, declared=true)
 public class NGCache implements Invalidateable {
 	/** the map backing the cache */
 	protected ConcurrentHashMap<String, SoftReference<Object>> cache = new ConcurrentHashMap<String, SoftReference<Object>>();
 	/** A count of the total number of invalidated cache entries */
 	protected AtomicLong invalidations = new AtomicLong(0);
 
+	
+
+
+
+	/**
+	 * Creates a new instance of a NGCache.
+	 */
+	public NGCache() {
+		
+	}
+	
+	
 	/**
 	 * Invalidates the cache entries identified by each of the passed keys.
 	 * @param keys The keys of the cache entries to invalidate.
 	 * @return an array of successfully invalidated keys.
 	 */
-	@Override
-	public String[] invalidate(String... keys) {
+	@JMXOperation (name="invalidate", description="Invalidates the cache entries identified by each of the passed keys.")
+	public String[] invalidate(
+			@JMXParameter(name="keys", description="The keys of the cache entries to invalidate.") String... keys) {
 		List<String> matches = new ArrayList<String>();
 		for(String key: keys) {
 			if(key!=null) {
@@ -55,4 +74,13 @@ public class NGCache implements Invalidateable {
 		invalidations.getAndAdd(matches.size());
 		return matches.toArray(new String[matches.size()]);
 	}
+	
+	/**
+	 * The total number of invalidations that have occurred since the cache started or the last reset.
+	 * @return the invalidations
+	 */
+	@JMXAttribute(name="Invalidations", description="The total number of invalidations that have occurred since the cache started or the last reset.", mutability=AttributeMutabilityOption.READ_ONLY)
+	public long getInvalidations() {
+		return invalidations.get();
+	}	
 }
