730a116d7afb36dd0d752016e34627d5553dba74
nwhitehead <nwhitehead@ff0b44bd-d83d-4c2c-bf2c-1e3224db2823>
2008-10-02 18:08:34 +0000

    Added some additional array processing static helper methods and exposed the real System out and err in final static variables.

--- nailgun/nailgun/src/java/prod/com/martiansoftware/nailgun/components/builtins/base/BaseComponentService.java
+++ nailgun/nailgun/src/java/prod/com/martiansoftware/nailgun/components/builtins/base/BaseComponentService.java
@@ -19,10 +19,14 @@
 package com.martiansoftware.nailgun.components.builtins.base;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.management.MBeanRegistration;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
+
+import EDU.oswego.cs.dl.util.concurrent.SynchronizedLong;
 
 /**
  * <p>Title:BaseComponentService</p>
@@ -35,8 +39,18 @@ public abstract class BaseComponentService implements MBeanRegistration {
 	protected MBeanServer server = null;
 	/** The JMX ObjectName of the cache's management interface */
 	protected ObjectName objectName = null;
-	protected static PrintStream out = null;
-	protected static PrintStream err = null;
+	public static final PrintStream out;
+	public static final PrintStream err;
+	
+	/** The total number of component calls */
+	protected SynchronizedLong totalComponentCalls = new SynchronizedLong(0);
+	/** The total number of failed component calls */
+	protected SynchronizedLong totalComponentErrors = new SynchronizedLong(0);	
+	/** The total elapsed time of component calls */
+	protected SynchronizedLong totalComponentCallTime = new SynchronizedLong(0);
+	
+	
+	
 	
 	static {
 		out = System.out;
@@ -44,6 +58,156 @@ public abstract class BaseComponentService implements MBeanRegistration {
 	}
 
 	
+	/**
+	 * Returns an array of strings containing all strings in the passed array that start with the provided prefix.
+	 * @param prefix The prefix to look for.
+	 * @param array The array to search.
+	 * @return A string array of matches.
+	 */
+	public static String[] getPrefixedStrings(String prefix, String[] array) {
+		if(array==null || array.length<1) return new String[0];
+		List matches = new ArrayList();
+		for(int i = 0; i < array.length; i++) {
+			if(array[i].startsWith(prefix)) {
+				matches.add(array[i]);
+			}
+		}
+		return (String[]) matches.toArray(new String[matches.size()]);
+	}
+	
+	/**
+	 * Removes matching strings from the passed array.
+	 * @param prefix The prefix to match strings against that will be removed from the array.
+	 * @param array The array to remove matches from.
+	 * @return The new array with matching strings removed.
+	 */
+	public static String[] removePrefixedStrings(String prefix, String[] array) {
+		if(array==null || array.length<1) return new String[0];
+		List matches = new ArrayList();
+		for(int i = 0; i < array.length; i++) {
+			if(!array[i].startsWith(prefix)) {
+				matches.add(array[i]);
+			}
+		}
+		return (String[]) matches.toArray(new String[matches.size()]);
+	}
+	
+	/**
+	 * Removes the prefix from each member of the array.
+	 * @param prefix The prefix to remove. 
+	 * @param array The array to remove prefixes from.
+	 * @return The prefix removed array.
+	 */
+	public static String[] removePrefixes(String prefix, String[] array) {
+		if(array==null || array.length<1) return new String[0];
+		for(int i = 0; i < array.length; i++) {
+			if(array[i].startsWith(prefix)) {
+				array[i] = array[i].substring(prefix.length());
+			}
+		}
+		return array;		
+	}
+	
+	/**
+	 * Appends the passed items to the passed array.
+	 * @param items The array of items to append.
+	 * @param array The array to append the items to.
+	 * @return The appended array.
+	 */
+	public static Object[] appendToArray(Object[] items, Object[] array) {
+		if(items==null || items.length < 1) return array;
+		Object[] newArr = new Object[array.length + items.length];
+		System.arraycopy(array, 0, newArr, 0, array.length);
+		System.arraycopy(items, 0, newArr, array.length, items.length);
+		return newArr;
+	}
+	
+	/**
+	 * Prefixes the passed items to the passed array.
+	 * @param items The array of items to prefix.
+	 * @param array The array to prefix the items to.
+	 * @return The prefixed array.
+	 */
+	public static Object[] prefixToArray(Object[] items, Object[] array) {
+		if(items==null || items.length < 1) return array;
+		Object[] newArr = new Object[array.length + items.length];
+		System.arraycopy(items, 0, newArr, 0, items.length);
+		System.arraycopy(array, 0, newArr, items.length, array.length);
+		return newArr;
+	}
+	
+	/**
+	 * Extracts the range of array items from the passed array and returns them in a new array.
+	 * Range indexes are inclusive.
+	 * @param array The array to extract from.
+	 * @param start The starting index of the range.
+	 * @param end The ending index of the range.
+	 * @return
+	 */
+	public static String[] extract(String[] array, int start, int end) {
+		if(array==null || array.length < 1) return array;
+		if(start<0) throw new RuntimeException("Start index is < 1");
+		if(end<0) throw new RuntimeException("End index is < 1");
+		if(end<start) throw new RuntimeException("Start index is greater than end index");
+		if(end>array.length-1) throw new RuntimeException("End index exceeds size of array");
+		String[] newArray = new String[end-start+1];
+		int x = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(i >= start && i <= end) {
+				newArray[x] = array[i];
+				x++;
+			}
+		}
+		return newArray;
+	}
+	
+	
+	/**
+	 * Reports the passed error string to the NGServer console and the client error stream.
+	 * @param message The error message
+	 * @param consoleErr The console output
+	 * @param clientErr The client output
+	 */
+	public static void reportError(String message, PrintStream consoleErr, PrintStream clientErr) {		
+		consoleErr.println(message);
+		clientErr.println("ERROR: " + message);
+	}
+	
+	
+	
+	/**
+	 * The calculated average elapsed time of successful component calls.
+	 * @return the average elapsed time of calls.
+	 */
+	public long getAverageComponentCallTime() {
+		return average(totalComponentCalls.get(), totalComponentCallTime.get());
+	}
+	
+	/**
+	 * The total number of successful component calls
+	 * @return the totalCalls
+	 */
+	public long getComponentTotalCalls() {
+		return totalComponentCalls.get();
+	}
+
+
+	/**
+	 * The total elapsed time of successful component  calls
+	 * @return the totalCallTime
+	 */
+	public long getComponentTotalCallTime() {
+		return totalComponentCallTime.get();
+	}
+
+
+	/**
+	 * The total number of errors encountered calling the component Service.
+	 * @return the totalErrors
+	 */
+	public long getComponentTotalErrors() {
+		return totalComponentErrors.get();
+	}
 	
 	
 	/**
@@ -184,6 +348,31 @@ public abstract class BaseComponentService implements MBeanRegistration {
 	}
 	
 	
+	/**
+	 * Calculates an integral percentage.
+	 * @param part
+	 * @param total
+	 * @return
+	 */
+	public static int percentage(long part, long total) {
+		float p = part;
+		float t = total;
+		float per = p/t*100;
+		return (int)per;
+	}
+	
+	/**
+	 * Calcualtes an integral average.
+	 * @param count
+	 * @param window
+	 * @return
+	 */
+	public static long average(long count, long window) {
+		float c = count;
+		float w = window;
+		float avg = window/count;
+		return (long)avg;
+	}
 	
 	
 	
